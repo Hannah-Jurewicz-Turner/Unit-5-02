e8df567f8b53c3a28aa4a79256c7f1d3281c9cbc
HannahJT <>
2021-06-01 14:53:54 +0000

    Added a comment to Java.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This makes it so that when you click the button, the text changes to my name.
 document.getElementById('button').addEventListener('click', displayMessage)
 
 function displayMessage () {
